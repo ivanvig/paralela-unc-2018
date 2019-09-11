79025fb95037c58c908b9ec93cfdc4551dff910b
Ivan Vignolles <ivanmvig@gmail.com>
2019-09-11 17:52:17 -0300

    scriptdelorto

---
+++ openmp/jupiter/engine/scriptdelorto.sh
@@ -0,0 +1,16 @@
+make clean > /dev/null
+make > /dev/null
+mkfifo fifocom
+# exec 3<> /tmp/asdasd
+(tail -f fifocom & (echo $!>/tmp/asdasd)) | OMP_NUM_THREADS="$1" ./jupiter &
+
+pid=$!
+echo "uci" >  fifocom
+echo "position startpos" >  fifocom
+echo "go depth 5" >  fifocom
+echo "quit" >  fifocom
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+wait $pid
+kill $(cat /tmp/asdasd)
+rm -f fifocom || true
+# pkill tail
