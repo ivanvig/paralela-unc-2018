4895ab2c1de919a9aa144f3ea5b5b20a5557c124
SorianoJuan <ju_soriano88@hotmail.com>
2019-10-09 18:16:46 -0300

    Makefile con xcompiler y main con CALL_CUDA

--- cuda/src/main.cu
+++ cuda/src/main.cu
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define LIST_SIZE 1610612736 //6 GB of ints
+// #define LIST_SIZE 1610612736 //6 GB of ints
+#define LIST_SIZE 100 //6 GB of ints
 #define BLOCK_SIZE 1024
-
-int * random_numbers_cuda = (int *) malloc(sizeof(int)*LIST_SIZE);
+#define CUDA_CALL(x) {cudaError_t cuda_error__ = (x); if (cuda_error__) printf("CUDA error: " #x " returned \"%s\"\n", cudaGetErrorString(cuda_error__));}
 
 __device__
 inline void SWAP(int32_t *_a,int32_t *_b){int32_t __aux; __aux = *_a; *_a = *_b; *_b = __aux;}
@@ -22,17 +22,18 @@ int main (){
     random_numbers[i] = rand()%20;
   }
 
+  int start_print = 0;
+  int n_prints = 20;
   printf("antes de gpu \n");
-  for (int i=0; i< 20; i++){
+  for (int i=start_print; i< start_print+n_prints; i++){
     printf("%i ", random_numbers[i]);
   }
   odd_even_bubble_sort(random_numbers, LIST_SIZE);
 
-  printf("despues de gpu \n");
-  for (int i=0; i< 20; i++){
-    printf("%i ", random_numbers_cuda[i]);
+  printf("\ndespues de gpu \n");
+  for (int i=start_print; i< start_print+n_prints; i++){
+    printf("%i ", random_numbers[i]);
   }
-  //assert_sorted()
 
   return 0;
 }
@@ -43,9 +44,11 @@ void odd_even_bubble_sort_kernel(int32_t * unsorted_list, int32_t list_size)
   int32_t block_id = blockDim.x * blockIdx.x;
   int32_t thread_id = threadIdx.x;
 
-  if (unsorted_list[(block_id+thread_id)*2] > unsorted_list[(block_id+thread_id)*2+1])
-    SWAP(&unsorted_list[(block_id+thread_id)*2], &unsorted_list[(block_id+thread_id)*2+1]);
-
+  if ((block_id+thread_id)*2<20){
+    unsorted_list[(block_id+thread_id)*2]=10;
+  }
+  // if (unsorted_list[(block_id+thread_id)*2] > unsorted_list[(block_id+thread_id)*2+1])
+  //   SWAP(&unsorted_list[(block_id+thread_id)*2], &unsorted_list[(block_id+thread_id)*2+1]);
 }
 
 
@@ -54,15 +57,18 @@ __host__
 void odd_even_bubble_sort(int32_t * list, int32_t list_size)
 {
   int32_t * cuda_dev;
-  cudaError_t error;
-  error = cudaMalloc(&cuda_dev, sizeof(int32_t)*list_size);
+  dim3 dimGrid ((uint)(LIST_SIZE/(2*BLOCK_SIZE)), 1, 1); //TODO: Usar ceil
+	dim3 dimBlock (BLOCK_SIZE, 1, 1);
 
-  cudaMemcpy(cuda_dev, list, sizeof(int32_t)*list_size, cudaMemcpyHostToDevice);
+  CUDA_CALL(cudaMalloc(&cuda_dev, sizeof(int32_t)*list_size));
 
-  /* odd_even_bubble_sort_kernel<<<(LIST_SIZE/(2*BLOCK_SIZE),1,1), (BLOCK_SIZE,1,1)>>>(list, list_size); */
-  odd_even_bubble_sort_kernel<<<(LIST_SIZE/(2*BLOCK_SIZE)), BLOCK_SIZE>>>(list, list_size);
+  CUDA_CALL(cudaMemcpy(cuda_dev, list, sizeof(int32_t)*list_size, cudaMemcpyHostToDevice));
 
-  cudaMemcpy(random_numbers_cuda, cuda_dev, sizeof(int32_t)*list_size, cudaMemcpyDeviceToHost);
+  odd_even_bubble_sort_kernel<<<dimGrid, dimBlock>>>(list, list_size);
+
+  CUDA_CALL(cudaDeviceSynchronize());
+
+  CUDA_CALL(cudaMemcpy(list, cuda_dev, sizeof(int32_t)*list_size, cudaMemcpyDeviceToHost));
 }
 
 
