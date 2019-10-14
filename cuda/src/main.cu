577ff154c4a49c61f0e74513a3e0a70015424434
SorianoJuan <ju_soriano88@hotmail.com>
2019-10-14 02:42:01 -0300

    cuda main con global y helloworld para test

--- cuda/src/main.cu
+++ cuda/src/main.cu
@@ -1,84 +1,90 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 // #define LIST_SIZE 1610612736 //6 GB of ints
-#define LIST_SIZE 100 //6 GB of ints
+//#define LIST_SIZE 209715200 //500 MB of ints
+#define LIST_SIZE 2048
 #define BLOCK_SIZE 1024
 #define CUDA_CALL(x) {cudaError_t cuda_error__ = (x); if (cuda_error__) printf("CUDA error: " #x " returned \"%s\"\n", cudaGetErrorString(cuda_error__));}
 
 __device__
 inline void SWAP(int32_t *_a,int32_t *_b){int32_t __aux; __aux = *_a; *_a = *_b; *_b = __aux;}
 
-int assert_sorted(int * list, int list_size);
-void odd_even_bubble_sort(int32_t * list, int32_t list_size);
+void odd_even_bubble_sort_global(int32_t * list, int32_t list_size);
+int assert_sorted (int * list, int list_size);
+
+__global__
+void koronel(int32_t * list, int32_t list_size)
+{
+  int64_t pos = 2*(blockDim.x * blockIdx.x + threadIdx.x);
+
+  for (int64_t i = 0; i<list_size; i++){
+    int64_t pos_oddeven = pos + (i&1);
+    if (pos_oddeven < list_size-1)
+      if(list[pos_oddeven]>list[pos_oddeven+1])
+        SWAP(&list[pos_oddeven], &list[pos_oddeven+1]);
+    __syncthreads();
+  }
+}
 
 int main (){
   srand(time(NULL));
 
   int * random_numbers = (int *) malloc(sizeof(int)*LIST_SIZE);
 
+  printf("Generando lista aleatoria de %i elementos\n", LIST_SIZE);
   for (int i = 0; i<LIST_SIZE; i++){
     random_numbers[i] = rand()%20;
   }
-
   int start_print = 0;
-  int n_prints = 20;
-  printf("antes de gpu \n");
+  int n_prints = 2048;
+  printf("Antes de gpu: Elementos desde %i hasta %i \n", start_print, start_print+n_prints);
   for (int i=start_print; i< start_print+n_prints; i++){
     printf("%i ", random_numbers[i]);
   }
-  odd_even_bubble_sort(random_numbers, LIST_SIZE);
+  printf("\n");
+  printf("Odd even bubble sort con memoria global \n");
+  odd_even_bubble_sort_global(random_numbers, LIST_SIZE);
 
-  printf("\ndespues de gpu \n");
+  printf("Chequeando si la lista esta ordenada... \n");
+  int elem;
+  if (elem = assert_sorted(random_numbers, LIST_SIZE))
+    printf("LISTA MAL ORDENADA EN ELEM N %i \n", elem);
+  else
+    printf("LISTA BIEN ORDENADA \n");
+
+  printf("Despues de gpu: Elementos desde %i hasta %i\n", start_print, start_print+n_prints);
   for (int i=start_print; i< start_print+n_prints; i++){
     printf("%i ", random_numbers[i]);
   }
+  printf("\n");
 
   return 0;
 }
 
-__global__
-void odd_even_bubble_sort_kernel(int32_t * unsorted_list, int32_t list_size)
-{
-  int32_t block_id = blockDim.x * blockIdx.x;
-  int32_t thread_id = threadIdx.x;
-
-  if ((block_id+thread_id)*2<20){
-    unsorted_list[(block_id+thread_id)*2]=10;
-  }
-  // if (unsorted_list[(block_id+thread_id)*2] > unsorted_list[(block_id+thread_id)*2+1])
-  //   SWAP(&unsorted_list[(block_id+thread_id)*2], &unsorted_list[(block_id+thread_id)*2+1]);
-}
-
-
-
 __host__
-void odd_even_bubble_sort(int32_t * list, int32_t list_size)
+void odd_even_bubble_sort_global (int32_t * list, int32_t list_size)
 {
-  int32_t * cuda_dev;
+  int32_t * device_list_ref;
+
   dim3 dimGrid ((uint)(LIST_SIZE/(2*BLOCK_SIZE)), 1, 1); //TODO: Usar ceil
 	dim3 dimBlock (BLOCK_SIZE, 1, 1);
 
-  CUDA_CALL(cudaMalloc(&cuda_dev, sizeof(int32_t)*list_size));
+  CUDA_CALL(cudaMalloc((void **) &device_list_ref, list_size*sizeof(int32_t)));
+  CUDA_CALL(cudaMemcpy(device_list_ref, list, list_size*sizeof(int32_t), cudaMemcpyHostToDevice));
 
-  CUDA_CALL(cudaMemcpy(cuda_dev, list, sizeof(int32_t)*list_size, cudaMemcpyHostToDevice));
+  printf("Llamando al kernel... \n");
+  koronel<<<dimGrid, dimBlock>>>(device_list_ref, list_size);
 
-  odd_even_bubble_sort_kernel<<<dimGrid, dimBlock>>>(list, list_size);
-
-  CUDA_CALL(cudaDeviceSynchronize());
-
-  CUDA_CALL(cudaMemcpy(list, cuda_dev, sizeof(int32_t)*list_size, cudaMemcpyDeviceToHost));
+  CUDA_CALL(cudaMemcpy(list, device_list_ref, list_size*sizeof(int32_t), cudaMemcpyDeviceToHost));
+  CUDA_CALL(cudaFree(device_list_ref));
 }
 
-
-
+__host__
 int assert_sorted (int * list, int list_size)
 {
   for (int i=0; i<list_size-1; i++){
     if (list[i] > list[i+1])
-      return 1;
+      return i+1;
   }
-
   return 0;
 }
