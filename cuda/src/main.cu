be89c41dede0bfd14169711995541f0edf4dc12e
SorianoJuan <ju_soriano88@hotmail.com>
2019-11-10 18:43:02 -0300

    cambiado a int8_t

--- cuda/src/main.cu
+++ cuda/src/main.cu
@@ -7,10 +7,12 @@
 //#define LIST_SIZE 209715200 //500 MB of ints
 // #define LIST_SIZE 1048576 // 1MB of ints
 // #define LIST_SIZE 65536
+// #define LIST_SIZE 49152
+#define LIST_SIZE 49152
 // #define LIST_SIZE 16384
 // #define LIST_SIZE (16384 + 8192)
 // #define LIST_SIZE (8192 + 4096 + 2048)
-#define LIST_SIZE (8192 + 4096)
+// #define LIST_SIZE (8192 + 4096)
 // #define LIST_SIZE (3*(8192 + 4096))
 // #define LIST_SIZE 8192
 // #define LIST_SIZE 4096
@@ -18,12 +20,12 @@
 #define CUDA_CALL(x) {cudaError_t cuda_error__ = (x); if (cuda_error__) printf("CUDA error: " #x " returned \"%s\"\n", cudaGetErrorString(cuda_error__));}
 
 __device__
-inline void SWAP(int32_t *_a,int32_t *_b){int32_t __aux; __aux = *_a; *_a = *_b; *_b = __aux;}
+inline void SWAP(int8_t *_a,int8_t *_b){int8_t __aux; __aux = *_a; *_a = *_b; *_b = __aux;}
 
-void odd_even_bubble_sort_global(int32_t * list, int32_t list_size);
-void odd_even_bubble_sort_shared(int32_t * list, int32_t list_size);
-int assert_sorted (int * list, int list_size);
-int list_to_file(const char *fname, int *buffer, size_t size);
+void odd_even_bubble_sort_global(int8_t * list, int32_t list_size);
+void odd_even_bubble_sort_shared(int8_t * list, int32_t list_size);
+int assert_sorted (int8_t * list, int list_size);
+int list_to_file(const char *fname, int8_t *buffer, size_t size);
 
 // __global__
 // void shared_koronel_64(int32_t * list, int32_t list_size, int8_t even)
@@ -72,13 +74,13 @@ int list_to_file(const char *fname, int *buffer, size_t size);
 // }
 
 __global__
-void shared_koronel(int32_t * list, int32_t list_size)
+void shared_koronel(int8_t * list, int32_t list_size)
 {
   // if (threadIdx.x == 0 && list_size < 10000) {
   //   printf("Primer elemento: %d\n", *list);
   // }
-  __shared__ int32_t slist[2*BLOCK_SIZE];
-  int32_t *win = (list + 2*(blockDim.x * blockIdx.x));
+  __shared__ int8_t slist[2*BLOCK_SIZE];
+  int8_t *win = (list + 2*(blockDim.x * blockIdx.x));
   int32_t win_size = 2*blockDim.x - (2*blockDim.x - list_size%(2*blockDim.x))*(((blockIdx.x+1)*2*blockDim.x) > list_size);
 
   if (2*threadIdx.x < win_size - 1) {
@@ -105,9 +107,9 @@ void shared_koronel(int32_t * list, int32_t list_size)
 }
 
 __global__
-void global_koronel(int32_t * list, int32_t list_size)
+void global_koronel(int8_t * list, int32_t list_size)
 {
-  int32_t *win = (list + 2*(blockDim.x * blockIdx.x));
+  int8_t *win = (list + 2*(blockDim.x * blockIdx.x));
   int32_t win_size = 2*blockDim.x - (2*blockDim.x - list_size%(2*blockDim.x))*(((blockIdx.x+1)*2*blockDim.x) > list_size);
   for (int32_t i = 0; i<win_size; i++){
     int32_t pos_oddeven = 2*threadIdx.x + (i&1);
@@ -121,17 +123,17 @@ void global_koronel(int32_t * list, int32_t list_size)
 int main (){
   srand(time(NULL));
 
-  int * random_numbers_global = (int *) malloc(sizeof(int)*LIST_SIZE);
-  int * random_numbers_shared = (int *) malloc(sizeof(int)*LIST_SIZE);
+  int8_t * random_numbers_global = (int8_t *) malloc(sizeof(int8_t)*LIST_SIZE);
+  int8_t * random_numbers_shared = (int8_t *) malloc(sizeof(int8_t)*LIST_SIZE);
 
   printf("Generando lista aleatoria de %i elementos\n", LIST_SIZE);
   for (int i = 0; i<LIST_SIZE; i++){
     // random_numbers_global[i] = rand()%20;
-    random_numbers_global[i] = LIST_SIZE - i;
+    random_numbers_global[i] = (int8_t) LIST_SIZE - i;
   }
-  list_to_file("sin_ordenar", random_numbers_global, sizeof(int)*LIST_SIZE);
+  list_to_file("sin_ordenar", random_numbers_global, sizeof(int8_t)*LIST_SIZE);
 
-  memcpy(random_numbers_shared, random_numbers_global, sizeof(int)*LIST_SIZE);
+  memcpy(random_numbers_shared, random_numbers_global, sizeof(int8_t)*LIST_SIZE);
   int start_print = 0;
   // int n_prints = 4096;
   int n_prints = LIST_SIZE;
@@ -189,14 +191,14 @@ int main (){
   } else
     printf("LISTA CON SHARED MEM BIEN ORDENADA \n");
 
-  list_to_file("ordenada", random_numbers_shared, sizeof(int)*LIST_SIZE);
+  list_to_file("ordenada", random_numbers_shared, sizeof(int8_t)*LIST_SIZE);
   return 0;
 }
 
 __host__
-void odd_even_bubble_sort_global (int32_t * list, int32_t list_size)
+void odd_even_bubble_sort_global (int8_t * list, int32_t list_size)
 {
-  int32_t * device_list_ref;
+  int8_t * device_list_ref;
   cudaEvent_t start, stop;
   CUDA_CALL(cudaEventCreate(&start));
   CUDA_CALL(cudaEventCreate(&stop));
@@ -205,8 +207,8 @@ void odd_even_bubble_sort_global (int32_t * list, int32_t list_size)
   // dim3 dimGrid (1, 1, 1); //TODO: Usar ceil
 	dim3 dimBlock (BLOCK_SIZE, 1, 1);
 
-  CUDA_CALL(cudaMalloc((void **) &device_list_ref, list_size*sizeof(int32_t)));
-  CUDA_CALL(cudaMemcpy(device_list_ref, list, list_size*sizeof(int32_t), cudaMemcpyHostToDevice));
+  CUDA_CALL(cudaMalloc((void **) &device_list_ref, list_size*sizeof(int8_t)));
+  CUDA_CALL(cudaMemcpy(device_list_ref, list, list_size*sizeof(int8_t), cudaMemcpyHostToDevice));
 
   printf("Llamando al kernel con global memory... \n");
   CUDA_CALL(cudaEventRecord(start));
@@ -223,14 +225,14 @@ void odd_even_bubble_sort_global (int32_t * list, int32_t list_size)
 
   printf("Tiempo en kernel de global (ms): %f\n", milliseconds/1000);
 
-  CUDA_CALL(cudaMemcpy(list, device_list_ref, list_size*sizeof(int32_t), cudaMemcpyDeviceToHost));
+  CUDA_CALL(cudaMemcpy(list, device_list_ref, list_size*sizeof(int8_t), cudaMemcpyDeviceToHost));
   CUDA_CALL(cudaFree(device_list_ref));
 }
 
 __host__
-void odd_even_bubble_sort_shared (int32_t * list, int32_t list_size)
+void odd_even_bubble_sort_shared (int8_t * list, int32_t list_size)
 {
-  int32_t * device_list_ref;
+  int8_t * device_list_ref;
   cudaEvent_t start, stop;
   CUDA_CALL(cudaEventCreate(&start));
   CUDA_CALL(cudaEventCreate(&stop));
@@ -248,8 +250,8 @@ void odd_even_bubble_sort_shared (int32_t * list, int32_t list_size)
   dim3 dimGrid(blocks_needed, 1, 1); //TODO: Usar ceil
 	dim3 dimBlock (BLOCK_SIZE, 1, 1);
 
-  CUDA_CALL(cudaMalloc((void **) &device_list_ref, list_size*sizeof(int32_t)));
-  CUDA_CALL(cudaMemcpy(device_list_ref, list, list_size*sizeof(int32_t), cudaMemcpyHostToDevice));
+  CUDA_CALL(cudaMalloc((void **) &device_list_ref, list_size*sizeof(int8_t)));
+  CUDA_CALL(cudaMemcpy(device_list_ref, list, list_size*sizeof(int8_t), cudaMemcpyHostToDevice));
 
   printf("Llamando al kernel con shared memory... \n");
   CUDA_CALL(cudaEventRecord(start));
@@ -259,7 +261,7 @@ void odd_even_bubble_sort_shared (int32_t * list, int32_t list_size)
 
         shared_koronel<<<dimGrid, dimBlock>>>
           (
-           device_list_ref + (i&1), 
+           device_list_ref + (i&1),
            LIST_SIZE - (i&1)
            );
   }
@@ -271,12 +273,12 @@ void odd_even_bubble_sort_shared (int32_t * list, int32_t list_size)
 
   printf("Tiempo en kernel de shared (ms): %f\n", milliseconds/1000);
 
-  CUDA_CALL(cudaMemcpy(list, device_list_ref, list_size*sizeof(int32_t), cudaMemcpyDeviceToHost));
+  CUDA_CALL(cudaMemcpy(list, device_list_ref, list_size*sizeof(int8_t), cudaMemcpyDeviceToHost));
   CUDA_CALL(cudaFree(device_list_ref));
 }
 
 __host__
-int assert_sorted (int * list, int list_size)
+int assert_sorted (int8_t * list, int list_size)
 {
   for (int i=0; i<list_size-1; i++){
     if (list[i] > list[i+1])
@@ -285,7 +287,7 @@ int assert_sorted (int * list, int list_size)
   return 0;
 }
 
-int list_to_file(const char *fname, int *buffer, size_t size)
+int list_to_file(const char *fname, int8_t *buffer, size_t size)
 {
   int fd = open(fname, O_WRONLY | O_CREAT);
   write(fd, buffer, size);
