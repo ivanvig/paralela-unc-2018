10d4332853083bfddd755d6537ece07d22ad7021
ivanvig <ivanmvig@gmail.com>
2019-11-11 17:20:53 -0300

    checkeo en python ahora usa int8

--- cuda/sorter.py
+++ cuda/sorter.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-sin_ordenar = np.fromfile('sin_ordenar', dtype=np.int32)
-ordenado = np.fromfile('ordenada', dtype=np.int32)
+sin_ordenar = np.fromfile('sin_ordenar', dtype=np.int8)
+ordenado = np.fromfile('ordenada', dtype=np.int8)
 
 ordenado_python = np.sort(sin_ordenar)
 
