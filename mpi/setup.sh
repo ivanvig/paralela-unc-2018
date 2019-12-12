b8c2764c44ca16fb708588d60f81e6b98d083e00
ivanvig <ivanmvig@gmail.com>
2019-12-12 01:28:11 -0300

    setup.sh

---
+++ mpi/setup.sh
@@ -0,0 +1,4 @@
+export TAU_OPTIONS=-optPdtCOpts=--c99
+export PATH=$PATH:$PWD/tau-2.29/x86_64/bin
+export TAU_MAKEFILE=$PWD/tau-2.29/x86_64/lib/Makefile.tau-mpi-pdt-openmp-profile-trace-mpitrace
+export TAU_TRACE=0
